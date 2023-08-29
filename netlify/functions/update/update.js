7dd311ceff84db9954eb11230a0b4c310658ab49
nawzaysfinah <nawzaysfinah@gmail.com>
2023-08-29 16:43:12 +0800

    Respond to messages

--- netlify/functions/update/update.js
+++ netlify/functions/update/update.js
@@ -5,10 +5,7 @@ exports.handler = async (event) => {
 
 const axios = require("axios").default;
 
-await axios.post(
-  `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-  {
-    chat_id: JSON.parse(event.body).message.chat.id,
-    text: "I got your message!",
-  }
-);
+axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+  chat_id: JSON.parse(event.body).message.chat.id,
+  text: "I got your message!",
+});
