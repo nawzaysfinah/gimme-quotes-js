7688587c64db9fca2143549705a8a79f16ed6c5b
nawzaysfinah <nawzaysfinah@gmail.com>
2023-08-29 16:33:33 +0800

    Telegram update function stub

---
+++ netlify/functions/update/update.js
@@ -0,0 +1,22 @@
+exports.handler = async (event) => {
+  console.log("Received an update from Telegram!", event.body);
+  return { statusCode: 200 };
+};
+
+// // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+// const handler = async (event) => {
+//   try {
+//     const subject = event.queryStringParameters.name || 'World'
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ message: `Hello ${subject}` }),
+//       // // more keys you can return:
+//       // headers: { "headerName": "headerValue", ... },
+//       // isBase64Encoded: true,
+//     }
+//   } catch (error) {
+//     return { statusCode: 500, body: error.toString() }
+//   }
+// }
+
+// module.exports = { handler }
