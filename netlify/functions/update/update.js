0c85e34b91582ba5aa4332a5e92ab2ac23c94288
nawzaysfinah <nawzaysfinah@gmail.com>
2023-08-29 17:45:46 +0800

    renamed bot

--- netlify/functions/update/update.js
+++ netlify/functions/update/update.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
   const { command, botName, extra } = messageParts(message.text);
 
-  if (botName === "partiibot" || botName === null) {
+  if (botName === "iamjusttestingbot" || botName === null) {
     switch (command) {
       case "echo":
         await sendMessage(message.chat.id, extra || "ECHO!");
