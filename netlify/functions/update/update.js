dd12b8fa8b9527467b2327e923fab0a8124443b4
nawzaysfinah <nawzaysfinah@gmail.com>
2023-08-29 18:18:38 +0800

    added start command

--- netlify/functions/update/update.js
+++ netlify/functions/update/update.js
@@ -10,8 +10,11 @@ exports.handler = async (event) => {
       case "echo":
         await sendMessage(message.chat.id, extra || "ECHO!");
         break;
+      case "start":
+        await sendMessage(message.chat.id, "alright partner, time to go!");
+        break;
       default:
-        await sendMessage(message.chat.id, "I don't understand that command.");
+        await sendMessage(message.chat.id, "Sorry I don't speak 'stupid'.");
     }
   }
 
