e1120ecc27d1370df68095a228e8359ab54a2fa0
nawzaysfinah <nawzaysfinah@gmail.com>
2023-10-03 15:01:42 +0800

    uncommented receiveQuote function

--- App.js
+++ App.js
@@ -7,15 +7,6 @@ const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf(TEST);
 
-// bot.command("start", (ctx) => {
-//   console.log(ctx.from);
-//   bot.telegram.sendMessage(
-//     ctx.chat.id,
-//     `hello there ${msg.from.first_name}!n/ Welcome to my new telegram bot.`,
-//     {}
-//   );
-// });
-
 bot.command("start", (msg) => {
   const chatId = msg.chat.id;
   bot.telegram.sendMessage(
