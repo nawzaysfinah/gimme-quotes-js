64620c1440a64b39e64731c028b4b1ae3bad2b5a
nawzaysfinah <nawzaysfinah@gmail.com>
2023-10-04 09:47:17 +0800

    added bot.command logic

--- netlify/functions/update/update.js
+++ netlify/functions/update/update.js
@@ -1,6 +1,12 @@
+TEST = "5767083831:AAElUmpfZ6v7ZLJX_yZ4PEgf7W8fI91bmJs"; // nawzaysfinah bot
+
 const sendMessage = require("../../../sendMessage");
 const messageParts = require("../../../messageParts");
 // const receiveQuote = require("../../../receiveQuote");
+
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(TEST);
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
@@ -17,6 +23,13 @@ exports.handler = async (event) => {
           message.chat.id,
           extra || "This will start your program"
         );
+        bot.command("start", (msg) => {
+          const chatId = msg.chat.id;
+          bot.telegram.sendMessage(
+            chatId,
+            `Hey ${msg.from.first_name}!\nWelcome to the Gimme Quotes Bot!`
+          );
+        });
         break;
       default:
         await sendMessage(message.chat.id, "Sorry I don't speak 'stupid'.");
