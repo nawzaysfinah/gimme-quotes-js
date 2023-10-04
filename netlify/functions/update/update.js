9f932eb3fb9abf3c16ed4e55704b4269b5da0a26
nawzaysfinah <nawzaysfinah@gmail.com>
2023-10-04 10:02:27 +0800

    back to previous

--- netlify/functions/update/update.js
+++ netlify/functions/update/update.js
@@ -12,13 +12,13 @@ exports.handler = async (event) => {
       case "echo":
         await sendMessage(message.chat.id, extra || "ECHO!");
         break;
-      case "start":
-        await sendMessage(
-          message.chat.id,
-          extra ||
-            `Hey ${msg.from.first_name}!\nWelcome to the Gimme Quotes Bot!`
-        );
-        break;
+      // case "start":
+      //   await sendMessage(
+      //     message.chat.id,
+      //     extra ||
+      //       `Hey ${msg.from.first_name}!\nWelcome to the Gimme Quotes Bot!`
+      //   );
+      //   break;
       default:
         await sendMessage(message.chat.id, "Sorry I don't speak 'stupid'.");
     }
