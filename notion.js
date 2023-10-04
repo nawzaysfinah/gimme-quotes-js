cabc1d093042b09d542b08a0284f1c18411e7366
nawzaysfinah <nawzaysfinah@gmail.com>
2023-10-04 16:32:51 +0800

    alternative api call

--- notion.js
+++ notion.js
@@ -4,26 +4,40 @@ const { Client } = require("@notionhq/client");
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-const options = {
-  method: "POST",
-  url: `https://api.notion.com/v1/databases/${process.env.DATABASE_ID}/query`,
-  headers: {
-    accept: "application/json",
-    "Notion-Version": "2022-06-28",
-    "content-type": "application/json",
-  },
-  data: { page_size: 100 },
+const get = async (query) => {
+  const res = await axios.post(
+    `https://api.notion.com/v1/databases/${process.env.DATABASE_ID}/query`,
+    { query }
+  );
+  return res.data.data;
 };
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+const quotes = res["properties"]["Quote"]["rich_text"][0]["plain_text"];
 
 module.exports = {
-  getQuotes: () => get(response),
+  getQuotes: () => get(quotes),
 };
+
+// const options = {
+//   method: "POST",
+//   url: `https://api.notion.com/v1/databases/${process.env.DATABASE_ID}/query`,
+//   headers: {
+//     accept: "application/json",
+//     "Notion-Version": "2022-06-28",
+//     "content-type": "application/json",
+//   },
+//   data: { page_size: 100 },
+// };
+
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
+
+// module.exports = {
+//   getQuotes: () => get(response),
+// };
