2c22700a17f39edb1693f4757b76e1c804a74ca8
nawzaysfinah <nawzaysfinah@gmail.com>
2023-10-04 16:05:47 +0800

    added Axios example from notion

---
+++ notion.js
@@ -0,0 +1,29 @@
+const axios = require("axios").default;
+
+const { Client } = require("@notionhq/client");
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+const options = {
+  method: "POST",
+  url: "https://api.notion.com/v1/databases/database_id/query",
+  headers: {
+    accept: "application/json",
+    "Notion-Version": "2022-06-28",
+    "content-type": "application/json",
+  },
+  data: { page_size: 100 },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+
+module.exports = {
+  getFeaturedPosts: () => get(featuredPosts),
+};
